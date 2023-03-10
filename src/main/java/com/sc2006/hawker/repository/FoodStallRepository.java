a1a40ce97085a2da8cdc0c9bef648ff2ddef820f
bc-chou <98440491+bc-chou@users.noreply.github.com>
2023-03-10 11:49:15 +0800

    changed directory for foodstall files

--- src/main/java/com/sc2006/hawker/repository/FoodStallRepository.java
+++ src/main/java/com/sc2006/hawker/repository/FoodStallRepository.java
@@ -1,5 +1,6 @@
-package com.sc2006.hawker;
+package com.sc2006.hawker.repository;
 
+import com.sc2006.hawker.model.FoodStall;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
