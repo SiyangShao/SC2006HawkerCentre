f7416f88bc9189351543b7e630cd88d3ac1c7c75
pufanyi <pufanyi@gmail.com>
2023-03-10 11:53:42 +0800

    update the change and delete method

--- src/main/java/com/sc2006/hawker/service/ReviewService.java
+++ src/main/java/com/sc2006/hawker/service/ReviewService.java
@@ -5,6 +5,7 @@ import com.sc2006.hawker.model.Review;
 import com.sc2006.hawker.repository.ReviewRepository;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
@@ -29,4 +30,40 @@ public class ReviewService {
 
         return review;
     }
+
+    /**
+     * Update review
+     * @param reviewBody - review body
+     * @param serialno - serial number of hawker
+     * @return - updated review
+     */
+    public Review updateReview(String reviewBody, String serialno) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Hawker.class)
+                .matching(Criteria.where("serialno").is(serialno))
+                .apply(new Update().push("reviews").value(review))
+                .first();
+
+        return review;
+    }
+
+
+    /**
+     * Delete review
+     * @param reviewBody - review body
+     * @param serialno - serial number of hawker
+     * @return - updated review
+     */
+    public Review deleteReview(String reviewBody, String serialno) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Hawker.class)
+                .matching(Criteria.where("serialno").is(serialno))
+                .apply(new Update().pull("reviews", Query.query(Criteria.where("id").is(review.getId()))))
+                .first();
+
+        return review;
+    }
+
 }
