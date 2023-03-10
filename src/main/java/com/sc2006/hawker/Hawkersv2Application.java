a3421edcc150be5c466874a7daaa313eb81daeda
darrennjk <darrennjk@gmail.com>
2023-03-10 11:08:55 +0800

    Updated with find by name, postalcode and nearest hawker.

---
+++ src/main/java/com/sc2006/hawker/Hawkersv2Application.java
@@ -0,0 +1,13 @@
+package com.sc2006.hawker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hawkersv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hawkersv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sc2006/hawker/Hawkersv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc2006.hawker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hawkersv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
