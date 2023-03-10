f75939408ba4d8338be05fa60335a93aec7ba6de
darrennjk <darrennjk@gmail.com>
2023-03-10 11:32:10 +0800

    Refactor Application Name

--- src/main/java/com/sc2006/hawker/HawkerApplication.java
+++ src/main/java/com/sc2006/hawker/HawkerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hawkersv2Application {
+public class HawkerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Hawkersv2Application.class, args);
+		SpringApplication.run(HawkerApplication.class, args);
 	}
 
 }
