35970f6b5aad6e9e238aa26afcb2ce896fc8a36e
William0498 <95912811+William0498@users.noreply.github.com>
2023-03-13 00:52:48 +0800

    Create User.java

---
+++ src/main/java/com/sc2006/hawker/model/User.java
@@ -0,0 +1,41 @@
+package com.sc2006.hawker.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "user")
+@Data
+@AllArgsConstructor
+//@NoArgsConstructor
+
+public class User {
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(min=3, max=20)
+    private String username;
+
+    @NotBlank
+    @Size(min=3, max=50)
+    private String email;
+
+    @NotBlank
+    @Size(min=3, max=40)
+    private String password;
+
+    public User(){
+    }
+
+    public User(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
