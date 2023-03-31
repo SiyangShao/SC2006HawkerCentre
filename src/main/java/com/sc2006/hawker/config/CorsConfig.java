f0ce019e0804674ea2fe3f24ef97a7deb6ce47fa
darrennjk <darrennjk@gmail.com>
2023-04-01 02:55:41 +0800

    Minor update to HawkerList.js

--- src/main/java/com/sc2006/hawker/config/CorsConfig.java
+++ src/main/java/com/sc2006/hawker/config/CorsConfig.java
@@ -3,7 +3,6 @@ package com.sc2006.hawker.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNullApi;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- src/main/webapp/reactjs/src/components/models/SingleHawkerCard.js
+++ src/main/webapp/reactjs/src/components/models/SingleHawkerCard.js
@@ -7,7 +7,6 @@ import "./SingleHawkerCard.css"
 
 // function for getting quarter # of current date
 let currentQuarter = (date) => {
-    // console.log(date)
     const currentMonth = date.getMonth();
 
     let currentQuarter;
