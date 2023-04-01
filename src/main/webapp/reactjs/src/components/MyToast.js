b988db66e731b51d5f033a841a79f76e5d8625a0
William0498 <95912811+William0498@users.noreply.github.com>
2023-04-01 17:33:23 +0800

    Create MyToast.js

---
+++ src/main/webapp/reactjs/src/components/MyToast.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {Toast} from 'react-bootstrap';
+
+export default class MyToast extends Component{
+    render() {
+        const toastCss = {
+            position: 'fixed',
+            top: '70px',
+            right:'20px',
+            zIndex:'1',
+            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+        };
+
+        return (
+            <div style={this.props.children.show ? toastCss : null}>
+                <Toast className={"border border-warning bg-warning text-white"}>
+                    <Toast.Header className={"bg-warning text-white"} closeButton={false}>
+                        <strong className="mr-auto">Error</strong>
+                    </Toast.Header>
+                    <Toast.Body>
+                        {this.props.children.message}
+                    </Toast.Body>
+                </Toast>
+            </div>
+        );
+    };
+}
