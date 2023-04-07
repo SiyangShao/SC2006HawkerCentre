8685edc99fad4cf5c22b206e1beff8e6ed703999
Pu Fanyi <pufanyi@gmail.com>
2023-04-07 12:43:58 +0800

    beautify review part
    please rerun npm install

--- src/main/webapp/reactjs/src/components/models/EditFoodStallReview.js
+++ src/main/webapp/reactjs/src/components/models/EditFoodStallReview.js
@@ -1,80 +1,8 @@
-// // import React, { useState } from "react";
-// //
-// // export default function EditHawkerCentreReview() {
-// //     const username = sessionStorage.getItem("username");
-// //     const hawkerID = sessionStorage.getItem("hawkerID");
-// //     const hawkerName = sessionStorage.getItem("hawkerName");
-// //     const [review, setReview] = useState("");
-// //
-// //     const handleSubmit = (event) => {
-// //         event.preventDefault();
-// //         // Here you can send the review to the server
-// //         console.log(`Username: ${username}, Hawker ID: ${hawkerID}, Hawker Name: ${hawkerName}, Review: ${review}`);
-// //     };
-// //
-// //     return (
-// //         <div>
-// //             <h1>Edit Hawker Centre Review</h1>
-// //             {/*<h2>Username: {username}</h2>*/}
-// //             {/*<h2>Hawker ID: {hawkerID}</h2>*/}
-// //             <h3>Hawker Name: {hawkerName}</h3>
-// //             <form onSubmit={handleSubmit}>
-// //                 <label>
-// //                     Review:
-// //                     <p>
-// //                     <textarea value={review} onChange={(e) => setReview(e.target.value)}/>
-// //                     </p>
-// //                 </label>
-// //                 <p>
-// //                 <button type="submit">Submit</button>
-// //                 </p>
-// //             </form>
-// //         </div>
-// //     );
-// //
-// // }
-//
-// import React, { useState } from "react";
-//
-// export default function EditHawkerCentreReview() {
-//     const username = sessionStorage.getItem("username");
-//     const hawkerID = sessionStorage.getItem("hawkerID");
-//     const hawkerName = sessionStorage.getItem("hawkerName");
-//     const [review, setReview] = useState("");
-//     const [rating, setRating] = useState("");
-//
-//     const handleSubmit = (event) => {
-//         event.preventDefault();
-//         // Here you can send the review and rating to the server
-//         console.log(`Username: ${username}, Hawker ID: ${hawkerID}, Hawker Name: ${hawkerName}, Review: ${review}, Rating: ${rating}`);
-//     };
-//
-//     return (
-//         <div>
-//             <h1>Edit Hawker Centre Review</h1>
-//             {/*<h2>Username: {username}</h2>*/}
-//             {/*<h2>Hawker ID: {hawkerID}</h2>*/}
-//             <h3>Hawker Name: {hawkerName}</h3>
-//             <form onSubmit={handleSubmit}>
-//                 <label>
-//                     Rating (out of 5):
-//                     <input type="number" value={rating} min="1" max="5" onChange={(e) => setRating(e.target.value)}/>
-//                 </label>
-//                 <br/>
-//                 <label>
-//                     Review:
-//                     <textarea value={review} onChange={(e) => setReview(e.target.value)}/>
-//                 </label>
-//                 <br/>
-//                 <button type="submit">Submit</button>
-//             </form>
-//         </div>
-//     );
-//
-// }
-
 import React, { useState } from "react";
 import axios from "axios";
+import { Card, Form, Button } from "react-bootstrap";
+import RatingStars from "react-rating-stars-component"; // 导入react-rating-stars-component
+import "./EditReview.css";
 
 export default function EditFoodStallReview() {
     const userName = sessionStorage.getItem("username");
@@ -95,41 +23,63 @@ export default function EditFoodStallReview() {
         console.log("rating: " + rating);
         const reviewBody = realReviewBody + "\nFor food stall: " + foodStallName;
         event.preventDefault();
-        axios.post("http://localhost:8080/api/v1/reviews/create/foodstall", { userName, hawkerSerialno, foodStallSerialno, hawkerName, reviewBody, rating })
-            .then(response => {
+        axios
+            .post("http://localhost:8080/api/v1/reviews/create/foodstall", {
+                userName,
+                hawkerSerialno,
+                foodStallSerialno,
+                hawkerName,
+                reviewBody,
+                rating,
+            })
+            .then((response) => {
                 console.log("Review created successfully:", response.data);
                 alert("Review created successfully");
                 // go back to previous page
                 window.history.back();
                 // You can navigate to another page or show a success message here
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error("Error creating review:", error);
                 // You can show an error message to the user here
             });
     };
 
     return (
-        <div>
-            <h1>Edit Hawker Centre Review</h1>
-            {/*<h2>Username: {username}</h2>*/}
-            {/*<h2>Hawker ID: {hawkerID}</h2>*/}
-            <h3>Hawker Name: {hawkerName}</h3>
-            <h3>Food Stall Name: {foodStallName}</h3>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Rating (out of 5):
-                    <input type="number" value={rating} min="1" max="5" onChange={(e) => setRating(e.target.value)} />
-                </label>
-                <br />
-                <label>
-                    Review:
-                    <textarea value={realReviewBody} onChange={(e) => setRealReviewBody(e.target.value)} />
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-            </form>
+        <div className="review-card-container">
+            <Card className="enter-review-card">
+                <h1>Edit Hawker Centre Review</h1>
+                <h3>Hawker Name: {hawkerName}</h3>
+                <h3>Food Stall Name: {foodStallName}</h3>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="rating">
+                        <Form.Label>Rating (out of 5):</Form.Label>
+                        {/* 使用react-rating-stars-component替代Form.Control，并添加样式 */}
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            <RatingStars
+                                count={5}
+                                size={24}
+                                activeColor="#ffd700"
+                                isHalf={false}
+                                value={rating}
+                                onChange={setRating}
+                            />
+                        </div>
+                    </Form.Group>
+                    <Form.Group controlId="reviewBody">
+                        <Form.Label>Review:</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            rows={4}
+                            value={realReviewBody}
+                            onChange={(e) => setRealReviewBody(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Card>
         </div>
     );
-
 }
--- src/main/webapp/reactjs/src/components/models/EditHawkerCentreReview.js
+++ src/main/webapp/reactjs/src/components/models/EditHawkerCentreReview.js
@@ -1,87 +1,16 @@
-// // import React, { useState } from "react";
-// //
-// // export default function EditHawkerCentreReview() {
-// //     const username = sessionStorage.getItem("username");
-// //     const hawkerID = sessionStorage.getItem("hawkerID");
-// //     const hawkerName = sessionStorage.getItem("hawkerName");
-// //     const [review, setReview] = useState("");
-// //
-// //     const handleSubmit = (event) => {
-// //         event.preventDefault();
-// //         // Here you can send the review to the server
-// //         console.log(`Username: ${username}, Hawker ID: ${hawkerID}, Hawker Name: ${hawkerName}, Review: ${review}`);
-// //     };
-// //
-// //     return (
-// //         <div>
-// //             <h1>Edit Hawker Centre Review</h1>
-// //             {/*<h2>Username: {username}</h2>*/}
-// //             {/*<h2>Hawker ID: {hawkerID}</h2>*/}
-// //             <h3>Hawker Name: {hawkerName}</h3>
-// //             <form onSubmit={handleSubmit}>
-// //                 <label>
-// //                     Review:
-// //                     <p>
-// //                     <textarea value={review} onChange={(e) => setReview(e.target.value)}/>
-// //                     </p>
-// //                 </label>
-// //                 <p>
-// //                 <button type="submit">Submit</button>
-// //                 </p>
-// //             </form>
-// //         </div>
-// //     );
-// //
-// // }
-//
-// import React, { useState } from "react";
-//
-// export default function EditHawkerCentreReview() {
-//     const username = sessionStorage.getItem("username");
-//     const hawkerID = sessionStorage.getItem("hawkerID");
-//     const hawkerName = sessionStorage.getItem("hawkerName");
-//     const [review, setReview] = useState("");
-//     const [rating, setRating] = useState("");
-//
-//     const handleSubmit = (event) => {
-//         event.preventDefault();
-//         // Here you can send the review and rating to the server
-//         console.log(`Username: ${username}, Hawker ID: ${hawkerID}, Hawker Name: ${hawkerName}, Review: ${review}, Rating: ${rating}`);
-//     };
-//
-//     return (
-//         <div>
-//             <h1>Edit Hawker Centre Review</h1>
-//             {/*<h2>Username: {username}</h2>*/}
-//             {/*<h2>Hawker ID: {hawkerID}</h2>*/}
-//             <h3>Hawker Name: {hawkerName}</h3>
-//             <form onSubmit={handleSubmit}>
-//                 <label>
-//                     Rating (out of 5):
-//                     <input type="number" value={rating} min="1" max="5" onChange={(e) => setRating(e.target.value)}/>
-//                 </label>
-//                 <br/>
-//                 <label>
-//                     Review:
-//                     <textarea value={review} onChange={(e) => setReview(e.target.value)}/>
-//                 </label>
-//                 <br/>
-//                 <button type="submit">Submit</button>
-//             </form>
-//         </div>
-//     );
-//
-// }
-
 import React, { useState } from "react";
 import axios from "axios";
+import { Card, Form, Button } from "react-bootstrap";
+import { FaStar } from "react-icons/fa";
+import "./EditReview.css";
+import RatingStars from "react-rating-stars-component";
 
 export default function EditHawkerCentreReview() {
     const userName = sessionStorage.getItem("username");
     const hawkerSerialno = sessionStorage.getItem("hawkerID");
     const hawkerName = sessionStorage.getItem("hawkerName");
     const [reviewBody, setReviewBody] = useState("");
-    const [rating, setRating] = useState("");
+    const [rating, setRating] = useState(0);
 
     const handleSubmit = (event) => {
         console.log("username: " + userName);
@@ -91,39 +20,64 @@ export default function EditHawkerCentreReview() {
         console.log("rating: " + rating);
 
         event.preventDefault();
-        axios.post("http://localhost:8080/api/v1/reviews/create/hawker", { userName, hawkerSerialno, hawkerName, reviewBody, rating })
-            .then(response => {
+        axios
+            .post("http://localhost:8080/api/v1/reviews/create/hawker", {
+                userName,
+                hawkerSerialno,
+                hawkerName,
+                reviewBody,
+                rating,
+            })
+            .then((response) => {
                 console.log("Review created successfully:", response.data);
                 alert("Review created successfully");
                 window.history.back();
                 // You can navigate to another page or show a success message here
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error("Error creating review:", error);
                 // You can show an error message to the user here
             });
     };
 
+    const handleRatingChange = (newRating) => {
+        setRating(newRating);
+    };
+
     return (
-        <div>
-            <h1>Edit Hawker Centre Review</h1>
-            {/*<h2>Username: {username}</h2>*/}
-            {/*<h2>Hawker ID: {hawkerID}</h2>*/}
-            <h3>Hawker Name: {hawkerName}</h3>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Rating (out of 5):
-                    <input type="number" value={rating} min="1" max="5" onChange={(e) => setRating(e.target.value)} />
-                </label>
-                <br />
-                <label>
-                    Review:
-                    <textarea value={reviewBody} onChange={(e) => setReviewBody(e.target.value)} />
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-            </form>
+        <div className="review-card-container">
+            <Card className="enter-review-card">
+                <h1>Edit Hawker Centre Review</h1>
+                <h3>Hawker Name: {hawkerName}</h3>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="rating">
+                        <Form.Label>Rating (out of 5):</Form.Label>
+                        {/* 使用react-rating-stars-component替代Form.Control，并添加样式 */}
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            <RatingStars
+                                count={5}
+                                size={24}
+                                activeColor="#ffd700"
+                                isHalf={false}
+                                value={rating}
+                                onChange={setRating}
+                            />
+                        </div>
+                    </Form.Group>
+                    <Form.Group controlId="review">
+                        <Form.Label>Review:</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            rows={3}
+                            value={reviewBody}
+                            onChange={(e) => setReviewBody(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Button type="submit" className="submit-btn">
+                        Submit
+                    </Button>
+                </Form>
+            </Card>
         </div>
     );
-
 }
