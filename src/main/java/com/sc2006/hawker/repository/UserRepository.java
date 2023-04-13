b5f5735cf9e18778e7b9028dda2e38826bf207e8
William0498 <95912811+William0498@users.noreply.github.com>
2023-04-14 00:42:10 +0800

    Update UserRepository.java

--- src/main/java/com/sc2006/hawker/repository/UserRepository.java
+++ src/main/java/com/sc2006/hawker/repository/UserRepository.java
@@ -11,6 +11,8 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findUserByUsername(String username);
+    
+    Optional<User> findUserByEmail(String email);
 
     Optional<User> findUserByUsernameAndPassword(String username, String password);
 
